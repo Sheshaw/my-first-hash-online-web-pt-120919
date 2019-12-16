21cc427d18d0d7356a3de9b88b63c236d2dfe078
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-16 04:34:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   cat = { "dog" => 20, "cat" => 50}
-  
+
 end
 
 
@@ -16,9 +16,10 @@ def retrieval
 end
 
 def adding
-  shipping_manifest["gun powder"] = 4
+  the_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest["gun powder"] = 4
+  the_manifest["muskets"] = 2
   the_manifest
-  
 
 end
 
